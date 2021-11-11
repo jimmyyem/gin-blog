72b073f9db89bbeaf200410e9b1a97567fef4482
yanhuaguo <yanhuaguo@zhangyue.com>
2021-11-11 15:19:40 +0800

    update start.sh

--- start.sh
+++ start.sh
@@ -1,3 +1,9 @@
 #!/bin/bash
 
-go generate && go build -o main.sh && ./main.sh
+WORK_DIR=$(cd `dirname $0`; pwd)
+echo $WORK_DIR
+GOMOD=$WORK_DIR/go.mod
+export GO111MODULE=auto
+export GOPATH=/home/ubuntu/go
+export GOROOT=/usr/local/go
+cd $WORK_DIR && go generate && go build -o main.sh && ./main.sh
